adc13035bd27c8fff0a181531592defd82ff0ca9
Pyry Lahtinen <pyry@pyry.info>
2024-11-09 16:24:05 +0200

    map with markers and lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { MapContainer, Marker, Polyline, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
+import { stations, lines } from './data'
 
-function App() {
-  return <h1>game here</h1>
+const App = () => {
+  const mapOptions = {
+    center: [60.1986580, 24.9334287],
+    zoom: 12,
+    maxZoom: 15,
+    minZoom: 10,
+  }
+
+  const markerIcon = L.icon({
+    iconUrl: 'unknown.png',
+    iconSize: [20, 20],
+    iconAnchor: [10, 10],
+  })
+
+  const pathOptions = {
+    color: '#ff0000',
+  }
+
+  return (
+    <MapContainer {...mapOptions} style={{ height: '500px' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        opacity={0.4}
+      />
+
+      {stations.map((stationCoords, index) =>
+        <Marker position={stationCoords} icon={markerIcon} key={index} />
+      )}
+
+      {lines.map((linePoints, index) =>
+        <Polyline pathOptions={pathOptions} positions={linePoints} key={index} />
+      )}
+    </MapContainer>
+  )
 }
 
 export default App
---
+++ src/data.js
@@ -0,0 +1,17 @@
+
+export const lines = [
+  // Pasila - Station
+  [
+    [60.1986580, 24.9334287],
+    [60.1717794, 24.9413548],
+  ],
+
+  // Others here
+]
+
+export const stations = [
+  // Pasila
+  [60.1986580, 24.9334287],
+
+  // Others here
+]
