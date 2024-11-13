022e662f13c39b9e0186f09fcb95ca599a7ffbfc
Pyry Lahtinen <pyry@pyry.info>
2024-11-13 18:32:07 +0200

    refactor map overlay to distinct component

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,11 @@
-import { useEffect, useState } from 'react'
 import Map from './Map'
-import { stations } from './data/stations'
-import { useRevealedStations } from './revealedStations'
+import MapOverlay from './MapOverlay'
 
 const App = () => {
-  const { revealedStations, revealStation, reset: resetRevealedStations } = useRevealedStations()
-  const [inputText, setInputText] = useState('')
-
-  const keyPressed = event => {
-    if (event.key !== 'Enter') {
-      return
-    }
-
-    const correctStation = stations.find(station => station.name.toLowerCase() === inputText.trim().toLowerCase())
-
-    if (correctStation !== undefined) {
-      revealStation(correctStation.name)
-      setInputText('')
-    } else {
-      if (!document.querySelector('.guess-input').classList.contains('incorrect')) {
-        document.querySelector('.guess-input').classList.add('incorrect')
-        setTimeout(() => document.querySelector('.guess-input').classList.remove('incorrect'), 200)
-      }
-    }
-  }
-
-  useEffect(() => {
-    document.querySelector('.guess-input').focus()
-  }, [])
-
   return (
     <div className='map-wrapper'>
       <Map />
-      <div className='map-overlay'>
-        <input
-          className='guess-input'
-          type='text' autoCapitalize='off' autoCorrect='off' autoComplete='off' maxLength={30}
-          value={inputText} onChange={e => setInputText(e.target.value)}
-          onKeyUp={keyPressed}
-          placeholder='Kirjoita aseman nimi...'
-        />
-        <div className='progress-pane'>
-          <button onClick={resetRevealedStations}>Tyhjennä</button>
-          <span className='score-label'>{revealedStations.length}/{stations.length}</span>
-        </div>
-      </div>
+      <MapOverlay />
     </div>
   )
 }
---
+++ src/MapOverlay.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { stations } from './data/stations'
+import { useRevealedStations } from './revealedStations'
+
+const MapOverlay = () => {
+  const { revealedStations, revealStation, reset: resetRevealedStations } = useRevealedStations()
+  const [inputText, setInputText] = useState('')
+
+  const keyPressed = event => {
+    if (event.key !== 'Enter') {
+      return
+    }
+
+    const correctStation = stations.find(station => station.name.toLowerCase() === inputText.trim().toLowerCase())
+
+    if (correctStation !== undefined) {
+      revealStation(correctStation.name)
+      setInputText('')
+    } else {
+      if (!document.querySelector('.guess-input').classList.contains('incorrect')) {
+        document.querySelector('.guess-input').classList.add('incorrect')
+        setTimeout(() => document.querySelector('.guess-input').classList.remove('incorrect'), 200)
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.querySelector('.guess-input').focus()
+  }, [])
+
+  return (
+    <div className='map-overlay'>
+      <input
+        className='guess-input'
+        type='text' autoCapitalize='off' autoCorrect='off' autoComplete='off' maxLength={30}
+        value={inputText} onChange={e => setInputText(e.target.value)}
+        onKeyUp={keyPressed}
+        placeholder='Kirjoita aseman nimi...'
+      />
+      <div className='progress-pane'>
+        <button onClick={resetRevealedStations}>Tyhjennä</button>
+        <span className='score-label'>{revealedStations.length}/{stations.length}</span>
+      </div>
+    </div>
+  )
+}
+
+export default MapOverlay
